b5826b784976fc43cc2528bd7fbb6a4a263ba983
Mohammad Ahmad <ma3571318z@gmail.com>
2021-06-15 16:04:08 +0300

    working on car booking, finish the header of home page

---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { BookCard } from "../../components/bookCard";
+import Navbar from "../../components/navbar";
+import { TopSection } from "./topSection";
+
+const PageContainer = styled.div`
+  ${tw`
+        flex
+        flex-col
+        w-full
+        h-full
+        items-center
+        overflow-x-hidden
+    `}
+`;
+
+const HomePage = () => {
+  return (
+    <PageContainer>
+      <Navbar />
+      <TopSection />
+      <BookCard />
+    </PageContainer>
+  );
+};
+
+export default HomePage;
